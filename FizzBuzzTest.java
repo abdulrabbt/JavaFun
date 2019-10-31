3915bcffe05f1d672c2f5c54b2ca63a85fb3bda9
abdulrabbt <abdulrab.bt@hotmail.com>
2019-10-31 21:54:15 +0300

    javaFun

---
+++ FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String fizzbuzz1 = fizzbuzz.fizzBuzz(15);
+        String fizzbuzz2 = fizzbuzz.fizzBuzz(20);
+        String fizzbuzz3 = fizzbuzz.fizzBuzz(21);
+        System.out.println(fizzbuzz1);
+        System.out.println(fizzbuzz2);
+        System.out.println(fizzbuzz3);
+    }
+}
